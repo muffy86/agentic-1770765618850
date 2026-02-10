3777731ab2d06a2351e7950ea0fb7e09d5605247
muffy <74996983+muffy86@users.noreply.github.com>
2026-02-10 17:20:20 -0600

    Add script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+let displayValue = '';
+
+function updateDisplay() {
+    document.getElementById('display').value = displayValue;
+}
+
+function appendNumber(number) {
+    displayValue += number;
+    updateDisplay();
+}
+
+function appendOperator(operator) {
+    displayValue += operator;
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '';
+    updateDisplay();
+}
+
+function backspace() {
+    displayValue = displayValue.slice(0, -1);
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue);
+        updateDisplay();
+    } catch (error) {
+        displayValue = 'Error';
+        updateDisplay();
+    }
+}
